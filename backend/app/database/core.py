b27c4020f0b56fafabb46f531585494296afaa65
konverner <konst.verner@gmail.com>
2025-07-20 12:23:34 +0200

    feat: create superuser via init db

--- backend/app/database/core.py
+++ backend/app/database/core.py
@@ -7,6 +7,8 @@ from typing import Generator
 
 from ..models import Base
 from ..config import settings
+from ..auth.service import AuthService
+from ..auth import schemas
 
 logger = logging.getLogger(__name__)
 
@@ -40,12 +42,39 @@ def init_db() -> None:
     try:
         Base.metadata.create_all(bind=engine)
         logger.info("Database initialized successfully.")
+        
+        # Create the first superuser if it doesn't exist
+        db_gen = get_db()
+        db = next(db_gen)
+        try:
+            auth_service = AuthService()
+            if not auth_service.get_user_by_username(db, settings.FIRST_SUPERUSER_USERNAME):
+                auth_service.create_user(
+                    db=db,
+                    user_in=schemas.UserCreate(
+                        username=settings.FIRST_SUPERUSER_USERNAME,
+                        password=settings.FIRST_SUPERUSER_PASSWORD,
+                        is_superuser=True,
+                        avatar_url=None
+                    )
+                )
+                logger.info(f"First superuser created with username: {settings.FIRST_SUPERUSER_USERNAME}")
+            else:
+                logger.info(f"Superuser with username {settings.FIRST_SUPERUSER_USERNAME} already exists.")
+        except Exception as user_creation_error:
+            logger.error(f"Error creating superuser: {user_creation_error}")
+        finally:
+            try:
+                db_gen.close()
+            except:
+                pass
     except Exception as e:
-        logger.info(f"Error initializing database: {e}")
+        logger.error(f"Error initializing database: {e}")
         raise
     finally:
         engine.dispose()
-        
+
+
 # Drop all tables
 def drop_db() -> None:
     try:
