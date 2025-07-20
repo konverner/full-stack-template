3cf827eece0ea14d8e83eab0a418cf61f4ce1cd2
konverner <konst.verner@gmail.com>
2025-07-20 12:15:20 +0200

    chore: refacto settings names

--- backend/app/config.py
+++ backend/app/config.py
@@ -26,18 +26,18 @@ def parse_cors(v: Any) -> list[str] | str:
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="../.env",
+        env_file="./.env",
         env_ignore_empty=True,
         extra="ignore",
     )
     HOST: str = "0.0.0.0"
-    PORT: int = 8000
+    PORT: int = 8080
     API_V1_STR: str = "/api/v1"
     AUTH_STR: str = "/auth"
     SECRET_KEY: str = secrets.token_urlsafe(32)
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    FRONTEND_HOST: str = "http://localhost:5173"
+    FRONTEND_HOST: str = "http://localhost:80"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
@@ -73,7 +73,7 @@ class Settings(BaseSettings):
 
 
     EMAIL_TEST_USER: EmailStr = "test@example.com"
-    FIRST_SUPERUSER: EmailStr
+    FIRST_SUPERUSER_USERNAME: str
     FIRST_SUPERUSER_PASSWORD: str
 
     def _check_default_secret(self, var_name: str, value: str | None) -> None:
