5cd0e960025cf92a4631b91dee4e9d79428d70e3
Konstantin Verner <konst.verner@gmail.com>
2026-01-14 11:14:47 +0100

    chore: rename local to dev

--- backend/app/config.py
+++ backend/app/config.py
@@ -81,7 +81,7 @@ class Settings(BaseSettings):
                 f'The value of {var_name} is "changethis", '
                 "for security, please change it, at least for deployments."
             )
-            if self.ENVIRONMENT == "local":
+            if self.ENVIRONMENT == "dev":
                 warnings.warn(message, stacklevel=1)
             else:
                 raise ValueError(message)
